6b2b1042cb33a03a73de374004196f0b6564e1af
Ali <qali372@gmail.com>
2024-03-28 14:40:56 +0200

    feat : testing with Main.java

--- src/main/java/com/seinfeld/Main.java
+++ src/main/java/com/seinfeld/Main.java
@@ -1,7 +1,39 @@
 package com.seinfeld;
 
+import com.seinfeld.dto.CarDTO;
+import com.seinfeld.dto.EmployeeDTO;
+import com.seinfeld.entity.BioDieselCar;
+import com.seinfeld.entity.Employee;
+import com.seinfeld.mapper.CarsMapper;
+import com.seinfeld.mapper.CarsMapperImpl;
+import com.seinfeld.mapper.EmployeeMapper;
+import com.seinfeld.mapper.EmployeeMapperImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        EmployeeMapper mapper =  new EmployeeMapperImpl();
+//
+//        EmployeeDTO dto = new EmployeeDTO();
+//        dto.setEmployeeId(1);
+//        dto.setEmployeeName("jerry");
+//        dto.setEmployeeStartAt("12-10-2022");
+//
+//        Employee entity = mapper.employeeDTOtoEmployee(dto);
+//
+//        System.out.println(entity.getId());
+//        System.out.println(entity.getName());
+//        System.out.println(entity.getStartAt());
+//
+//        BioDieselCar bioDieselCar = new BioDieselCar();
+//        bioDieselCar.setId(1);
+//        bioDieselCar.setName("van");
+//
+//        CarsMapper mapper1 = new CarsMapperImpl();
+//
+//        CarDTO carDTO = mapper1.toCarDto(bioDieselCar);
+//
+//        System.out.println(carDTO.getId());
+//        System.out.println(carDTO.getName());
+//        System.out.println(carDTO.getFuelType());
     }
 }
